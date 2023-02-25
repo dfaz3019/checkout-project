8f72710e6b90d6029c308226ee2de4d1678361bb
Dana Fazeli <danafazeli@hotmail.co.uk>
2023-02-25 23:18:37 +1100

    Disabling buttons for no input

--- script.js
+++ script.js
@@ -6,37 +6,33 @@ const quantityFieldFive = document.querySelector('.quantity-field-5')
 const quantityFieldSix = document.querySelector('.quantity-field-6')
 
 document.querySelector('#calculate-button').addEventListener('click', () => {
-    if(Number(quantityFieldOne.value) == -1) {
+    if (Number(quantityFieldOne.value) == -1) {
         alert('Please select a positive value quantity')
+        quantityFieldOne.focus()
     } else {
-    document.querySelector('.cart-total').innerHTML = ""
-    document.querySelector('.cart-total').append(`Total is: $${Math.round(quantityFieldOne.value * 18.52 * 100) / 100}`)
+        document.querySelector('.cart-total').innerHTML = ""
+        document.querySelector('.cart-total').append(`Total is: $${Math.round(quantityFieldOne.value * 18.52 * 100) / 100}`)
     }
 })
 
-const fullName = document.querySelector('.name')
-const address = document.querySelector('.address')
+const fullName = document.querySelector('.text-box-name')
+const address = document.querySelector('.text-box-address')
 const country = document.querySelector('.country-dropdown-menu')
 const fieldShippingDetails = document.querySelector('.shipping-details')
+const calculateButton = document.querySelector('#calculate-button')
+const confirmAddressButton = document.querySelector('#confirm-address-button')
 
-document.querySelector('#confirm-address-button').addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () => {
+    if (fullName.value === "") {
+        confirmAddressButton.disabled = true;
+    } else {
+        confirmAddressButton.disabled = false;
+    }
+})
+
+document.querySelector('#confirm-address-button').addEventListener('click', (e) => {
+    e.preventDefault()
     const confirmAddress = `<p>We will send this package to ${fullName.value} at ${address.value}, ${country.value}</p>`
     const confirmationLabel = document.querySelector('.address-confirmation-message').innerHTML = confirmAddress
-})
-
-document.querySelector('.text-box-name').addEventListener('mouseenter', () => {
-    document.querySelector('.text-box-name').style.backgroundColor = 'lightgray'
-})
-
-document.querySelector('.text-box-name').addEventListener('mouseleave', () => {
-    document.querySelector('.text-box-name').style.backgroundColor = 'white'
-})
-
-document.querySelector('.text-box-address').addEventListener('mouseenter', () => {
-    document.querySelector('.text-box-address').style.backgroundColor = 'lightgray'
-})
-
-document.querySelector('.text-box-address').addEventListener('mouseleave', () => {
-    document.querySelector('.text-box-address').style.backgroundColor = 'white'
 })
 
