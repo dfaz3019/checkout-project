9e7aa0e1a9573999017095a4d296dee2e7347d1d
Dana Fazeli <danafazeli@hotmail.co.uk>
2023-02-26 01:44:07 +1100

    Made JS more readable + price total function

--- script.js
+++ script.js
@@ -4,22 +4,13 @@ const quantityFieldThree = document.querySelector('.quantity-field-3')
 const quantityFieldFour = document.querySelector('.quantity-field-4')
 const quantityFieldFive = document.querySelector('.quantity-field-5')
 const quantityFieldSix = document.querySelector('.quantity-field-6')
-
-document.querySelector('#calculate-button').addEventListener('click', () => {
-    if (Number(quantityFieldOne.value) == -1) {
-        alert('Please select a positive value quantity')
-        quantityFieldOne.focus()
-    } else {
-        document.querySelector('.cart-total').innerHTML = ""
-        document.querySelector('.cart-total').append(`Total is: $${Math.round(quantityFieldOne.value * 18.52 * 100) / 100}`)
-    }
-})
 const fullName = document.querySelector('.text-box-name')
 const address = document.querySelector('.text-box-address')
 const country = document.querySelector('.country-dropdown-menu')
 const fieldShippingDetails = document.querySelector('.shipping-details')
 const calculateButton = document.querySelector('#calculate-button')
 const confirmAddressButton = document.querySelector('#confirm-address-button')
+const resetCart = document.querySelector('#reset-cart')
 
 fullName.addEventListener("input", () => {
     if (fullName.value === "") {
@@ -38,9 +29,35 @@ fullName.addEventListener("blur", () => {
     }
 })
 
-document.querySelector('#confirm-address-button').addEventListener('click', (e) => {
+confirmAddressButton.addEventListener('click', (e) => {
     e.preventDefault()
     const confirmAddress = `<p>We will send this package to ${fullName.value} at ${address.value}, ${country.value}</p>`
     const confirmationLabel = document.querySelector('.address-confirmation-message').innerHTML = confirmAddress
 })
 
+function calculatePrice (quantityField, cost) {
+    if (Number(quantityField.value) == -1) {
+        alert('Please select a positive value quantity')
+        quantityField.focus()
+    } else {
+        return Math.round(quantityField.value * cost * 100) / 100
+    }
+}
+
+calculateButton.addEventListener('click', () => {
+    const cartTotalLabel = document.querySelector('.cart-total')
+    cartTotal = 
+    `Total: $${calculatePrice(quantityFieldOne, 18.52) + calculatePrice(quantityFieldTwo, 15.00) + 
+     calculatePrice(quantityFieldThree, 31.52) + calculatePrice(quantityFieldFour, 9.82) + 
+     calculatePrice(quantityFieldFive, 14.82) + calculatePrice(quantityFieldSix, 6.82)}`
+     cartTotalLabel.innerHTML = cartTotal
+})
+
+resetCart.addEventListener('click', () => {
+    quantityFieldOne.value = 0 
+    quantityFieldTwo.value = 0 
+    quantityFieldThree.value = 0 
+    quantityFieldFour.value = 0 
+    quantityFieldFive.value = 0 
+    quantityFieldSix.value = 0 
+})
