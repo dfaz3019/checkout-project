3f1c01a6f2f06364e51cea3254d7b0b074fd4ff3
Andrew Miller <andrew.william.miller7@gmail.com>
2023-02-26 01:43:07 +1300

    added blur event listener and disabled keyword in html.

--- script.js
+++ script.js
@@ -22,13 +22,21 @@ const fieldShippingDetails = document.querySelector('.shipping-details')
 const calculateButton = document.querySelector('#calculate-button')
 const confirmAddressButton = document.querySelector('#confirm-address-button')
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
+//     if (fullName.value === "") {
+//         confirmAddressButton.disabled = true;
+//     } else {
+//         confirmAddressButton.disabled = false;
+//     }
+// })    
+
+fullName.addEventListener("blur", () => {
     if (fullName.value === "") {
         confirmAddressButton.disabled = true;
     } else {
         confirmAddressButton.disabled = false;
     }
-})    
+})
 
 document.querySelector('#confirm-address-button').addEventListener('click', (e) => {
     e.preventDefault()
